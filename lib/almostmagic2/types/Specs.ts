6a3e06fb34f6235f4723c1d08fe6f08efc840886
Vova Zakharov <vzakharov@gmail.com>
2023-08-15 19:49:49 +0300

    more cha-cha-cha

--- lib/almostmagic2/types/Specs.ts
+++ lib/almostmagic2/types/Specs.ts
@@ -1,21 +1,7 @@
 export type Specs = string | string[] | Record<string, string>;
 
-export type ModelOutput<O extends Specs> = 
-  O extends string 
-    ? InferTypeFromValue<O>
-  : 
-  O extends string[] 
-    ? {
-      [K in O[number]]: InferTypeFromKey<K>;
-    }
-  : O extends Record<string, string> 
-    ? {
-      [K in keyof O]: 
-        InferTypeFromSpec<O, K>;
-    } : never;
-
 export type FitsTemplate<
-Exact extends string | undefined, Prefix extends string | undefined, Suffix extends string | undefined
+  Exact extends string | undefined, Prefix extends string | undefined, Suffix extends string | undefined
 > =
   ( Exact extends string ? Exact : never )
   | ( Prefix extends string ? `${Prefix}${string}` : never )
@@ -35,51 +21,74 @@ export const fitsTemplateChecker = <
   (str: string): str is FitsTemplate<Exact, Prefix, Suffix> =>
     str === exact || !!prefix && startsWith(str, prefix) || !!suffix && endsWith(str, suffix);
 
-export type NumberSpecValue = FitsTemplate<'number', 'number ', ' (number)'>;
-export type BooleanSpecValue = FitsTemplate<'boolean', 'true if ', ' (boolean)'>;
-export type BooleanSpecKey = FitsTemplate<undefined, `is${Capitalize<string>}`, undefined>;
-export type ArrayOfNumbersSpecValue = FitsTemplate<undefined, 'array of numbers', ' (array of numbers)'>;
-export type ArrayOfStringsSpecValue = Exclude<
-  FitsTemplate<undefined, 'array of ', ' (array of strings)'>,
-  ArrayOfNumbersSpecValue
->;
-export type ArrayOfStringsSpecKey = FitsTemplate<undefined, `${string}Array`, undefined>;
+export type SpecValue = {
+  number: FitsTemplate<'number', 'number ', ' (number)'>;
+  boolean: FitsTemplate<'boolean', 'true if ', ' (boolean)'>;
+  numberArray: FitsTemplate<undefined, 'array of numbers', ' (array of numbers)'>;
+  stringArray: FitsTemplate<undefined, 'list of ', ' (array of strings)'>
+  // (We had to use "list of" instead of "array of" because then it would work for "array of numbers" as well, as it's not possible to define a TypeScript type that would allow us to distinguish between the two.)
+};
+
+export type SpecKey = {
+  boolean: FitsTemplate<undefined, `is${Capitalize<string>}`, undefined>;
+  stringArray: FitsTemplate<undefined, `${string}Array`, undefined>;
+};
+
+export type SpecType = {
+  number: number;
+  boolean: boolean;
+  numberArray: number[];
+  stringArray: string[];
+  string: string;
+}
+
+export type InferTypeFromKey<K extends string> = {
+  [P in keyof SpecKey]: K extends SpecKey[P] ? SpecType[P] : never;
+}[keyof SpecKey];
+
+type TestInferTypeFromKey = InferTypeFromKey<'isPaid'>; // expected: boolean
+type TestInferTypeFromKey2 = InferTypeFromKey<'notesArray'>; // expected: string[]
+type TestInferTypeFromKey3 = InferTypeFromKey<'groceries'>; // expected: never
+
+export type InferTypeFromValue<V extends string> = {
+  [P in keyof SpecValue]: Lowercase<V> extends SpecValue[P] ? SpecType[P] : never;
+}[keyof SpecValue];
+
+type TestInferTypeFromValue = InferTypeFromValue<'number'>; // expected: number
+type TestInferTypeFromValue2 = InferTypeFromValue<'true if paid'>; // expected: boolean
+type TestInferTypeFromValue3 = InferTypeFromValue<'array of numbers'>; // expected: number[]
+type TestInferTypeFromValue4 = InferTypeFromValue<'list of items to buy'>; // expected: string[]
 
 
-export type InferTypeFromKey<K extends string> =
-  K extends BooleanSpecKey
-    ? boolean
-  : K extends ArrayOfStringsSpecKey
-    ? string[]
-  : never;
-
-export type InferTypeFromValue<V extends string> =
-  Lowercase<V> extends NumberSpecValue
-    ? number
-  : Lowercase<V> extends BooleanSpecValue
-    ? boolean
-  : Lowercase<V> extends ArrayOfNumbersSpecValue
-    ? number[]
-  : Lowercase<V> extends ArrayOfStringsSpecValue
-    ? string[]
-  : never;
-
-export type InferTypeFromSpec<O extends Record<string, string>, K extends keyof O> =
-  K extends string
-    ? InferTypeFromKey<K> extends never
-      ? InferTypeFromValue<O[K]> extends never
+export type ModelOutput<O extends Specs> = 
+  O extends string 
+    ? InferTypeFromValue<O> extends never
+      ? string
+      : InferTypeFromValue<O>
+  : 
+  O extends string[] 
+    ? {
+      [K in O[number]]: InferTypeFromKey<K> extends never
         ? string
-        : InferTypeFromValue<O[K]>
-      : InferTypeFromKey<K>
-    : never;
+        : InferTypeFromKey<K>;
+    }
+  : O extends Record<string, string> 
+    ? {
+      [K in keyof O]: 
+        K extends string
+          ? InferTypeFromKey<K> extends never
+            ? InferTypeFromValue<O[K]> extends never
+              ? string
+              : InferTypeFromValue<O[K]>
+            : InferTypeFromKey<K>
+          : never;
+    } : never;
 
 export const valueRepresentsNumber = fitsTemplateChecker('number', 'number ', ' (number)');
 export const valueRepresentsBoolean = fitsTemplateChecker('boolean', 'true if ', ' (boolean)');
 export const keyRepresentsBoolean = fitsTemplateChecker(undefined, 'is', undefined);
 export const valueRepresentsNumberArray = fitsTemplateChecker(undefined, 'array of numbers', ' (array of numbers)');
-export const valueRepresentsStringArray = (value: string): value is ArrayOfStringsSpecValue =>
-  !valueRepresentsNumberArray(value)
-    && fitsTemplateChecker(undefined, 'array of ', ' (array of strings)')(value);
+export const valueRepresentsStringArray = fitsTemplateChecker(undefined, 'list of ', ' (array of strings)');
 export const keyRepresentsStringArray = fitsTemplateChecker(undefined, undefined, 'Array');
 
 export type GenerateOutput<O extends Specs> = ModelOutput<O>;
@@ -90,13 +99,23 @@ export const modelToGenerateOutput = <O extends Specs>(modelOutput: ModelOutput<
     : modelOutput 
 ) as GenerateOutput<O>;
 
-type TestOutputs = ModelOutput<{
+type TestSpecs = {
   groceries: 'list of items to buy';
   unitPrices: 'unit prices for all items (array of numbers)';
   total: 'amount to pay (number)';
   isPaid: 'true if paid';
-  notesArray: 'arbitrary notes';
-}>;
+  notes: 'arbitrary notes';
+}
+
+type TestOutputs = ModelOutput<TestSpecs>;
+
+type TestSpecs2 = ['groceriesArray', 'isPaid', 'notes'];
+
+type TestOutputs2 = ModelOutput<TestSpecs2>;
+
+type TestSpec3 = 'List of items to buy';
+
+type TestOutputs3 = ModelOutput<TestSpec3>;
 
 // 
 // type TestOutputs = {
