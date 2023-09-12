0acfcc7a7c717aff4e47a6ff9c3d0a37999f9fda
Vova Zakharov <vzakharov@gmail.com>
2023-09-12 16:45:57 +0300

    blink

--- pages/jobgenie/index.vue
+++ pages/jobgenie/index.vue
@@ -3,8 +3,14 @@
     <div class="flex flex-col md:flex-row items-start md:items-center justify-center min-h-screen p-5">
       <div class="w-full p-2 md:w-1/4 md:mr-6 fixed top-0 left-0 h-screen overflow-auto">
         <ul class="space-y-2">
-          <li v-for="section in sections" :key="section" class="cursor-pointer hover:bg-gray-200 p-2 rounded" @click="selectedSection = section">
-            {{ section }}
+          <li v-for="section in sections" :key="section" class="cursor-pointer hover:bg-gray-200 p-2 rounded"   
+            @click="selectedSection = section"
+          >
+            <span 
+              v-if="section==='DNA'"
+              v-text="dnaSet ? 'DNA ☜': 'DNA'"
+            />
+            <span v-else v-text="section" />
           </li>
         </ul>
       </div>
@@ -27,6 +33,7 @@
   import { data } from '~/components/jobgenie/data';
   import Chat from '~/components/jobgenie/Chat/Chat.vue';
   import Login from '~/components/jobgenie/Login.vue';
+  import { dnaSet } from '~/components/jobgenie/dna';
 
   const process = useWindowProcess();
 
