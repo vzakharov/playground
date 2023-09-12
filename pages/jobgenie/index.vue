cf770dbc9913cf14f9ad5806302b8bb5e9b8e868
Vova Zakharov <vzakharov@gmail.com>
2023-09-12 16:41:13 +0300

    select sections

--- pages/jobgenie/index.vue
+++ pages/jobgenie/index.vue
@@ -3,15 +3,16 @@
     <div class="flex flex-col md:flex-row items-start md:items-center justify-center min-h-screen p-5">
       <div class="w-full p-2 md:w-1/4 md:mr-6 fixed top-0 left-0 h-screen overflow-auto">
         <ul class="space-y-2">
-          <li v-for="section in sections" :key="section" class="cursor-pointer hover:bg-gray-200 p-2 rounded">
-            {{ _.startCase(section) }}
+          <li v-for="section in sections" :key="section" class="cursor-pointer hover:bg-gray-200 p-2 rounded" @click="selectedSection = section">
+            {{ section }}
           </li>
         </ul>
       </div>
       <div class="w-full md:w-3/4 md:ml-64">
         <Login v-if="!data.username || !process.env.OPENAI_API_KEY" @="{ login }" />
         <template v-else>
-          <Chat type="interview" />
+          <Chat v-if="selectedSection === 'Interview'" type="interview" />
+          <!-- Add more sections here -->
         </template>
       </div>
     </div>
@@ -20,20 +21,24 @@
 
 <script setup lang="ts">
 
-import _ from 'lodash';
-import { Credentials } from '~/components/jobgenie/Credentials';
-import { data } from '~/components/jobgenie/data';
-import Chat from '~/components/jobgenie/Chat/Chat.vue';
-import Login from '~/components/jobgenie/Login.vue';
+  import _ from 'lodash';
+  import { ref } from 'vue';
+  import { Credentials } from '~/components/jobgenie/Credentials';
+  import { data } from '~/components/jobgenie/data';
+  import Chat from '~/components/jobgenie/Chat/Chat.vue';
+  import Login from '~/components/jobgenie/Login.vue';
 
-const process = useWindowProcess();
+  const process = useWindowProcess();
 
-function login(c: Credentials) {
-  data.username = c.username;
-  process.env.OPENAI_API_KEY = c.apiKey;
-}
+  function login(c: Credentials) {
+    data.username = c.username;
+    process.env.OPENAI_API_KEY = c.apiKey;
+  }
 
-// Add your sections here
-const sections = ['interview'];
+  // Add your sections here
+  const sections = ['Interview', 'DNA'] as const;
+
+  // Selected section
+  const selectedSection = ref<typeof sections[number]>(sections[0]);
 
 </script>
