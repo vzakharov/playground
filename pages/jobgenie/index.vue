febedbce246b6e0113d1d1be244d55417ebcc198
Vova Zakharov <vzakharov@gmail.com>
2023-09-12 16:54:19 +0300

    section objects

--- pages/jobgenie/index.vue
+++ pages/jobgenie/index.vue
@@ -1,23 +1,20 @@
 <template>
   <div class="container mx-auto px-4 pb-20">
     <div class="flex flex-col md:flex-row items-start md:items-center justify-center min-h-screen p-5">
-      <div class="w-full p-2 md:w-1/4 md:mr-6 fixed top-0 left-0 h-screen overflow-auto">
+      <div class="w-full p-2 md:w-1/6 md:mr-6 fixed top-0 left-0 h-screen overflow-auto">
         <ul class="space-y-2">
-          <li v-for="section in sections" :key="section" class="cursor-pointer hover:bg-gray-200 p-2 rounded"   
-            @click="selectedSection = section"
+          <li v-for="section in sections.filter(s => s.include)"
+            :key="section.id" class="cursor-pointer hover:bg-gray-200 p-2 rounded"   
+            @click="selectedSection = section.id"
           >
-            <span 
-              v-if="section==='DNA'"
-              v-text="dnaSet ? 'DNA ☜': 'DNA'"
-            />
-            <span v-else v-text="section" />
+            <span v-text="section.caption" />
           </li>
         </ul>
       </div>
-      <div class="w-full md:w-3/4 md:ml-64">
+      <div class="w-full md:w-5/6 md:ml-64">
         <Login v-if="!data.username || !process.env.OPENAI_API_KEY" @="{ login }" />
         <template v-else>
-          <Chat v-if="selectedSection === 'Interview'" type="interview" />
+          <Chat v-if="selectedSection === 'interview'" type="interview" />
           <!-- Add more sections here -->
         </template>
       </div>
@@ -28,7 +25,7 @@
 <script setup lang="ts">
 
   import _ from 'lodash';
-  import { ref } from 'vue';
+  import { ref, computed } from 'vue';
   import { Credentials } from '~/components/jobgenie/Credentials';
   import { data } from '~/components/jobgenie/data';
   import Chat from '~/components/jobgenie/Chat/Chat.vue';
@@ -43,9 +40,20 @@
   }
 
   // Add your sections here
-  const sections = ['Interview', 'DNA'] as const;
+  const sections = computed(() => [
+    {
+      id: 'interview',
+      caption: 'Interview',
+      include: true
+    },
+    {
+      id: 'dna',
+      caption: dnaSet.value ? 'DNA ☜': 'DNA',
+      include: !!data.dna
+    }
+  ]);
 
   // Selected section
-  const selectedSection = ref<typeof sections[number]>(sections[0]);
+  const selectedSection = ref(sections.value[0].id);
 
 </script>
