1592765f1bf49a41a55e6a5ba08202904a9cdd6a
Vova Zakharov <vzakharov@gmail.com>
2023-09-13 16:57:56 +0300

    separate Sidebar

---
+++ components/shared/Sidebar.vue
@@ -0,0 +1,41 @@
+<template>
+  <button class="hamburger" @click="isVisible = !isVisible">
+    <span class="hamburger-line"></span>
+    <span class="hamburger-line"></span>
+    <span class="hamburger-line"></span>
+  </button>
+  <div class="sidebar" :class="{ 'hidden lg:block': !isVisible, 'block': isVisible }">
+    <div class="upper">
+      <slot name="upper"/>
+    </div>
+    <div class="lower">
+      <slot name="lower"/>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const isVisible = ref(false);
+
+</script>
+
+<style scoped lang="postcss">
+
+.hamburger {
+  @apply lg:hidden absolute left-2 top-2;
+}
+
+.hamburger-line {
+  @apply block w-6 h-1 bg-gray-500 my-1 rounded;
+}
+
+.sidebar {
+  @apply md:w-full shadow md:w-1/6 md:mr-6 min-w-max fixed lg:top-0 top-10 left-0 h-screen overflow-auto bg-white lg:block;
+}
+
+.lower {
+  @apply absolute bottom-10 lg:bottom-0 w-full text-sm p-2 text-gray-400;
+}
+
+</style>
--- pages/jobgenie/index.vue
+++ pages/jobgenie/index.vue
@@ -7,6 +7,7 @@ import { data } from '~/components/jobgenie/data';
 import Toggle from '~/components/shared/Toggle.vue';
 import { sections, selectedSection } from './sections';
 import { usdSpent, useGpt4 } from './utils';
+import Sidebar from '~/components/shared/Sidebar.vue';
 
 const process = useWindowProcess();
 
@@ -15,33 +16,31 @@ function login(c: Credentials) {
   process.env.OPENAI_API_KEY = c.apiKey;
 }
 
-const isSidebarVisible = ref(false);
-
 </script>
 
 <template>
   <div class="container">
-    <button class="hamburger" @click="isSidebarVisible = !isSidebarVisible">
-      <span class="hamburger-line"></span>
-      <span class="hamburger-line"></span>
-      <span class="hamburger-line"></span>
-    </button>
-    <div class="sidebar" :class="{'hidden lg:block': !isSidebarVisible, 'block': isSidebarVisible}">
-      <ul class="menu">
-        <li v-for="section in sections.filter(s => s.include !== false)" :key="section.id" :class="`
+    <Sidebar>
+      <template #upper>
+        <ul>
+          <li v-for="section in sections.filter(s => s.include !== false)" :key="section.id" 
+            :class="`
               menu-item
               ${section.id === selectedSection.id && 'selected'}
-                                ${section.disabled && 'disabled'}
-            `" @click="!section.disabled && (selectedSection = section)"
-          :title="section.disabled ? section.disabled : ''">
-          <span v-text="`${section.emoji} ${section.caption}`" />
-        </li>
-      </ul>
-      <div class="status">
+              ${section.disabled && 'disabled'}
+            `"
+            @click="!section.disabled && (selectedSection = section)"
+            :title="section.disabled ? section.disabled : ''"
+          >
+            <span v-text="`${section.emoji} ${section.caption}`" />
+          </li>
+        </ul>
+      </template>
+      <template #lower>
         <Toggle v-model="useGpt4" label="GPT-4" title="This is around 10x more expensive if turned on." />
         Total spent: ${{ Math.round(usdSpent * 100) / 100 }}
-      </div>
-    </div>
+      </template>
+    </Sidebar>
     <div class="content">
       <Login v-if="!data.username || !process.env.OPENAI_API_KEY" @="{ login }" />
       <template v-else>
@@ -65,10 +64,6 @@ const isSidebarVisible = ref(false);
 .sidebar {
   @apply md:w-full shadow md:w-1/6 md:mr-6 min-w-max fixed lg:top-0 top-10 left-0 h-screen overflow-auto;
 }
-
-/* .menu {
-    @apply space-y-1;
-  } */
 
 .menu-item {
   @apply cursor-pointer p-2 rounded text-gray-700 hover:text-gray-900;
