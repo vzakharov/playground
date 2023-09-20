2591e261abf4bcfa6f238d68000e4d0c94e38164
Vova Zakharov <vzakharov@gmail.com>
2023-09-20 21:13:20 +0300

    move styles

--- components/shared/TextModal.vue
+++ components/shared/TextModal.vue
@@ -29,14 +29,44 @@ function cancel() {
 </script>
 
 <template>
-  <div class="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-    <div class="bg-white rounded-lg p-8">
-      <h2 class="text-lg font-medium mb-4">{{ title }}</h2>
-      <textarea class="w-full border-gray-300 rounded-lg p-2 mb-4" v-model="text"></textarea>
-      <div class="flex justify-end">
-        <button class="bg-gray-300 text-gray-700 rounded-lg px-4 py-2 mr-2" @click="cancel">Cancel</button>
-        <button class="bg-blue-500 text-white rounded-lg px-4 py-2" @click="submit">{{ buttonText }}</button>
+  <div class="modal-overlay">
+    <div class="modal-content">
+      <h2 class="modal-title">{{ title }}</h2>
+      <textarea class="modal-textarea"></textarea>
+      <div class="modal-buttons">
+        <button class="cancel-button" @click="cancel">Cancel</button>
+        <button class="submit-button" @click="submit">{{ buttonText }}</button>
       </div>
     </div>
   </div>
 </template>
+
+<style scoped lang="postcss">
+.modal-overlay {
+  @apply fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center;
+}
+
+.modal-content {
+  @apply bg-white rounded-lg p-8;
+}
+
+.modal-title {
+  @apply text-lg font-medium mb-4;
+}
+
+.modal-textarea {
+  @apply w-full border-gray-300 rounded-lg p-2 mb-4;
+}
+
+.modal-buttons {
+  @apply flex justify-end;
+}
+
+.cancel-button {
+  @apply bg-gray-300 text-gray-700 rounded-lg px-4 py-2 mr-2;
+}
+
+.submit-button {
+  @apply bg-blue-500 text-white rounded-lg px-4 py-2;
+}
+</style>
