c832419db9ec91a2ac63ba200b07d4cc25731599
Vova Zakharov <vzakharov@gmail.com>
2023-09-27 12:44:47 +0300

    use NODE_ENV for generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,15 +5,10 @@ export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
   ],
-  app: {
-    baseURL: process.env.BASE_URL ?? '/',
-    buildAssetsDir: 'assets',
-  },
-  // routeRules: {
-  //   // Redirect root to /jobgenie
-  //   '/': {
-  //     redirect: '/jobgenie',
-  //   },
-  // },
-  // (Doesn't work with statically generated pages)
+  ...process.env.NODE_ENV === 'production' && {
+    app: {
+      baseURL: '/playground',
+      buildAssetsDir: 'assets',
+    },
+  }
 })
