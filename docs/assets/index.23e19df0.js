63c0a34af39d61bc463a7b511268a314e0d68865
Vova Zakharov <vzakharov@gmail.com>
2023-10-02 08:30:56 +0300

    generate

--- docs/assets/index.23e19df0.js
+++
@@ -1,1 +0,0 @@
-import{d as e,o,a as n,l as a,k as t}from"./entry.b6563fa3.js";const _=e({__name:"index",setup(r){return o(()=>{n().replace("/jobgenie")}),(s,c)=>(t(),a("div"))}});export{_ as default};
---
+++ docs/assets/index.af795536.js
@@ -0,0 +1,1 @@
+import{d as e,o,a as n,l as a,k as t}from"./entry.90aa7b45.js";const _=e({__name:"index",setup(r){return o(()=>{n().replace("/jobgenie")}),(s,c)=>(t(),a("div"))}});export{_ as default};
