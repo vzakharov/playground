3924a0eab355e64f1ec82f2edd0dd8fa6070f80d
Vova Zakharov <vzakharov@gmail.com>
2023-10-17 11:49:53 +0300

    move BaseChatController

---
+++ lib/genie/BaseChatController.ts
@@ -0,0 +1,66 @@
+import _ from 'lodash';
+import { isBy } from '~/lib/vovas-openai';
+import { AssetName, ChatId, GenieChat, GenieChatType, GenieData, GenieMessage, GenieState, PromptBuilder, editMessage, findOrCreateChat, says } from '.';
+import { ChatControllerState } from './controller';
+
+
+export type BaseChatControllerConfig<
+  Ts extends GenieChatType,
+  T extends Ts,
+  A extends AssetName
+> = {
+  data: GenieData<Ts>;
+  globalState: GenieState;
+  type: T;
+  chatId: ChatId;
+  state: ChatControllerState<A>;
+  promptBuilder: PromptBuilder<T, any, any>;
+  autoMessage?: (data: GenieData<Ts>) => GenieMessage<A, 'assistant'>;
+};
+
+export class BaseChatController<
+  Ts extends GenieChatType,
+  T extends Ts,
+  A extends AssetName
+> {
+
+  constructor(
+    public readonly config: BaseChatControllerConfig<Ts, T, A>
+  ) {
+    this.chat = findOrCreateChat(config.data, config.type, config.chatId);
+    this.messages = this.chat.messages;
+  };
+
+  readonly chat: GenieChat<T, A>;
+  readonly messages: GenieMessage<A>[];
+  previousGeneration?: GenieMessage<A, 'assistant'>;
+
+  get lastMessageIsFromUser() {
+    const { messages } = this;
+    const lastMessage = _.last(messages);
+    return lastMessage && isBy.user(lastMessage);
+  };
+
+  removeMessagesFrom(message: GenieMessage<A>) {
+    const { messages } = this;
+    messages.splice(messages.indexOf(message), messages.length - messages.indexOf(message));
+  };
+
+  editMessage = editMessage;
+
+  regenerate(message: GenieMessage<A, 'assistant'>) {
+    this.previousGeneration = message;
+    this.removeMessagesFrom(message);
+  };
+
+  sendMessage() {
+    const { messages, config: { state } } = this;
+    const content = state.userMessage.trim();
+    if (content.trim() !== '') {
+      messages.push(says.user(content));
+      state.userMessage = '';
+    }
+  };
+
+}
+;
--- lib/genie/controller.ts
+++ lib/genie/controller.ts
@@ -1,10 +1,8 @@
 import _ from 'lodash';
 import { ToRefs } from 'vue';
 import { Resolvable, toReactive } from '~/lib/utils';
-import { isBy } from '~/lib/vovas-openai';
 import {
-  AssetName, ChatId, GenieChat, GenieChatType, GenieData, GenieMessage, GenieState, PromptBuilder, Responder, editMessage, findBy, findOrCreateChat, says
-} from '.';
+  AssetName, GenieChatType, GenieMessage, Responder, findBy} from '.';
 
 export type ChatControllerState<A extends AssetName> = {
   generating: Resolvable<GenieMessage<A, 'assistant'>> | undefined;
@@ -13,69 +11,6 @@ export type ChatControllerState<A extends AssetName> = {
     textarea?: HTMLTextAreaElement;
   } | undefined;
   msExpected: number | undefined;
-};
-
-export type BaseChatControllerConfig<
-  Ts extends GenieChatType, 
-  T extends Ts, 
-  A extends AssetName
-> = {
-  data: GenieData<Ts>;
-  globalState: GenieState;
-  type: T;
-  chatId: ChatId;
-  state: ChatControllerState<A>;
-  promptBuilder: PromptBuilder<T, any, any>;
-  autoMessage?: ( data: GenieData<Ts> ) => GenieMessage<A, 'assistant'>;
-};
-
-export class BaseChatController<  
-  Ts extends GenieChatType, 
-  T extends Ts, 
-  A extends AssetName
-> {
-
-  constructor(
-    public readonly config: BaseChatControllerConfig<Ts, T, A>
-  ) {
-    this.chat = findOrCreateChat(config.data, config.type, config.chatId );
-    this.messages = this.chat.messages;
-  };
-
-  readonly chat: GenieChat<T, A>;
-  readonly messages: GenieMessage<A>[];
-  previousGeneration?: GenieMessage<A, 'assistant'>;
-
-  get lastMessageIsFromUser() {
-    const { messages } = this;
-    const lastMessage = _.last(messages);
-    return lastMessage && isBy.user(lastMessage);
-  };
-
-  removeMessagesFrom(message: GenieMessage<A>) {
-    const { messages } = this;
-    messages.splice(messages.indexOf(message), messages.length - messages.indexOf(message));
-  };
-
-  editMessage = editMessage;
-
-  regenerate(message: GenieMessage<A, 'assistant'>) {
-    this.previousGeneration = message;
-    this.removeMessagesFrom(message);
-  };
-
-  sendMessage() {
-    const { messages, config: { state }} = this;
-    const content = state.userMessage.trim();
-    if (content.trim() !== '') {
-      messages.push(says.user(content));
-      state.userMessage = '';
-    }
-  };
-
-  // countIrrelevantMessages,
-  // isRelevant,
-    
 };
 
 export class ChatController<Ts extends GenieChatType, T extends Ts, A extends AssetName> 
