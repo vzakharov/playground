c0be740f945915739396cc630c6d4e1453050579
Vova Zakharov <vzakharov@gmail.com>
2023-10-22 16:32:40 +0300

    tsdocs for bound tools

--- lib/genie/tooling/BoundTool.ts
+++ lib/genie/tooling/BoundTool.ts
@@ -1,13 +1,30 @@
 import { AssetForTool, ChatController, Genie, GenieConfig, Requires, ToolFrom, Toolset } from "..";
 import { Tool } from "./Tool";
 
+/**
+ * A type alias for any BoundTool instance.
+ */
 export type AnyBoundTool = BoundTool<Toolset, ToolFrom<Toolset>>;
 
+/**
+ * The BoundTool class represents a tool that is bound to a specific Genie instance.
+ * A bound tool has access to the Genie's configuration and active assets, and can
+ * create chat controllers that are also bound to the same Genie.
+ *
+ * @template S The type of the toolset that the Genie is configured with.
+ * @template T The type of the tool that is being bound.
+ */
 export class BoundTool<
   S extends Toolset,
   T extends ToolFrom<S>
 > extends Tool<T['id'], AssetForTool<T>, Requires<T>> {
 
+  /**
+   * Creates a new BoundTool instance.
+   *
+   * @param tool The tool to bind.
+   * @param genie The Genie to bind the tool to.
+   */
   constructor(
     tool: T,
     public genie: Genie<S>
@@ -16,8 +33,17 @@ export class BoundTool<
     super(id, config);
   };
 
+  /**
+   * An array of chat controllers that are bound to this tool.
+   */
   chatControllers: ChatController<this>[] = [];
 
+  /**
+   * Creates a new chat controller that is bound to this tool.
+   *
+   * @param config The configuration for the chat controller.
+   * @returns The new chat controller.
+   */
   chatController(config: Omit<ChatController<this>['config'], 'tool' | keyof GenieConfig<S>>) {
     return new ChatController({
       ...config,
@@ -26,6 +52,11 @@ export class BoundTool<
     });
   };
 
+  /**
+   * Returns an array of the required assets that are currently missing from the Genie's active assets.
+   *
+   * @returns An array of missing required assets.
+   */
   get missingRequires() {
     return this.getMissingRequires(this.genie.activeAssets);
   };
