bb79d3d068c93d0470b75f691d2ea33e000edfe4
Vova Zakharov <vzakharov@gmail.com>
2023-10-27 10:59:06 +0300

    abstract data

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,65 +1,88 @@
+<script setup lang="ts">
+
+type Section = { title: string, text?: string, subsections?: Section[] };
+
+const cv = ref({
+  header: {
+    title: 'Vova Zakharov',
+    subtitle: 'AI Tinkerer • Coding Fanatic • Word Alchemist',
+  },
+  sections: [
+    {
+      title: 'Bio',
+      text: 'Hey there! I\'ve been messing around with tech, AI, and words for over 20 years. Sometimes, I mix them all up and see what comes out. I find joy in creating projects that stir curiosity, challenge norms, and ultimately, make a difference.',
+    },
+    {
+      title: 'Experience',
+      subsections: [
+        {
+          title: 'Voicemod (Experience and Innovation team), January-September 2023',
+          text: 'Developed prototypes for Voicemod\'s experimental ideas. Built a Discord bot - the bot-making process was as simple as writing in YAML. Cobbled together a Python-based web API resembling chatgpt\'s web interface, but operating with a local python model for generation.',
+        },
+        {
+          title: 'Personal projects (Since 2020)',
+          text: '',
+          subsections: [
+            {
+              title: 'Jukebox Web UI',
+              text: 'Created a web UI built on top of Gradio and runs on Google Colab. An innovative approach using Google Colab as an actual server.',
+            },
+            {
+              title: '“almostmagic” package',
+              text: 'Simplified the integration of LLMs into projects. Just one line of code and it feels almost like magic.',
+            },
+            {
+              title: 'JobGenie - An AI-powered job creation tool',
+              text: 'A revolutionary AI tool that doesn\'t just assist in job searches but invents unique job roles. Welcome to JobGenie.',
+            },
+          ],
+        },
+        {
+          title: '2006-2023: Copywriter & Diverse Roles in Marketing, Sales, and Tech Support',
+          text: 'Throughout these roles, I consistently leveraged self-developed software tools to enhance and automate various aspects of my work, highlighting my passion for blending technology with traditional job functions.',
+        },
+      ],
+    },
+    {
+      title: 'Education',
+      subsections: [
+        {
+          title: 'Moscow Institute of Physics and Technology',
+          text: 'Master, Engineering Physics/Applied Physics (2000 - 2006)',
+        },
+      ],
+    },
+  ],
+});
+
+</script>
+
 <template>
   <div class="container">
     <!-- Header -->
     <div class="header">
       <div>
-        <h1 class="title">Vova Zakharov</h1>
-        <p class="subtitle">AI Tinkerer • Coding Fanatic • Word Alchemist</p>
+        <h1 class="title" v-text="cv.header.title"></h1>
+        <p class="subtitle" v-text="cv.header.subtitle"></p>
       </div>
     </div>
 
-    <!-- Bio -->
-    <div class="section">
-      <h2 class="section-title">Bio</h2>
-      <p class="section-text">Hey there! I've been messing around with tech, AI, and words for over 20 years.
-        Sometimes, I mix them all up and see what comes out. I find joy in creating projects that stir curiosity,
-        challenge norms, and ultimately, make a difference.</p>
-    </div>
-
-    <!-- Experience -->
-    <div class="section">
-      <h2 class="section-title">Experience</h2>
-      <div class="subsection">
-        <h3 class="subsection-title">Voicemod (Experience and Innovation team), January-September 2023</h3>
-        <p class="section-text">Developed prototypes for Voicemod's experimental ideas. Built a Discord bot - the
-          bot-making process was as simple as writing in YAML. Cobbled together a Python-based web API resembling
-          chatgpt's web interface, but operating with a local python model for generation.</p>
+    <!-- Sections -->
+    <div class="section" v-for="(section, index) in cv.sections" :key="index">
+      <h2 class="section-title" v-text="section.title"></h2>
+        <p class="section-text" v-if="section.text" v-text="section.text"></p>
+        <div class="subsection" v-if="section.subsections" v-for="(sub, subIndex) in section.subsections" :key="`sub-${subIndex}`">
+          <h3 class="subsection-title" v-text="sub.title"></h3>
+          <p class="section-text" v-if="sub.text" v-text="sub.text"></p>
+          <div class="subsubsection" v-if="sub.subsections" v-for="(subsub, subsubIndex) in sub.subsections" :key="`subsub-${subsubIndex}`">
+            <h4 class="subsubsection-title" v-text="subsub.title"></h4>
+            <p class="section-text" v-if="subsub.text" v-text="subsub.text"></p>
+          </div>
+        </div>
       </div>
-      <div class="subsection">
-        <h3 class="subsection-title">Personal projects (Since 2020)</h3>
-
-        <h4 class="subsubsection-title">Jukebox Web UI</h4>
-        <p class="section-text">Created a web UI built on top of Gradio and runs on Google Colab. An innovative
-          approach using Google Colab as an actual server.</p>
-
-        <h4 class="subsubsection-title">“almostmagic” package</h4>
-        <p class="section-text">Simplified the integration of LLMs into projects. Just one line of code and it feels
-          almost like magic.</p>
-
-        <h4 class="project-title">JobGenie - An AI-powered job creation tool</h4>
-        <p class="section-text">A revolutionary AI tool that doesn't just assist in job searches but invents unique
-          job roles. Welcome to JobGenie.</p>
-      </div>
-      <div class="subsection">
-        <h3 class="subsection-title">2006-2023: Copywriter & Diverse Roles in Marketing, Sales, and Tech Support
-        </h3>
-        <p class="section-text">Throughout these roles, I consistently leveraged self-developed software tools to
-          enhance and automate various aspects of my work, highlighting my passion for blending technology with
-          traditional job functions.</p>
-      </div>
-    </div>
-
-    <!-- Education -->
-    <div class="section">
-      <h2 class="section-title">Education</h2>
-      <div class="subsection">
-        <h3 class="subsection-title">Moscow Institute of Physics and Technology</h3>
-        <p class="section-text">Master, Engineering Physics/Applied Physics (2000 - 2006)</p>
-      </div>
-    </div>
   </div>
 </template>
-  
+
 <style lang="postcss">
 .container {
   @apply max-w-4xl mx-auto p-6 bg-white rounded;
@@ -100,5 +123,4 @@
 .subsubsection-title {
   @apply text-lg font-bold mt-4;
 }
-
 </style>
