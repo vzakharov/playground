948da163a94c2b4e7c9aec4a22e08508c696603b
Vova Zakharov <vzakharov@gmail.com>
2023-10-31 08:16:42 +0300

    Create Agent.ts

---
+++ lib/autogen/agentchat/Agent.ts
@@ -0,0 +1,63 @@
+import { Message } from "./Message";
+
+/**
+ * Options for sending and receiving messages.
+ */
+export type SendReceiveOptions = {
+  /** Whether to request a reply from the recipient. */
+  requestReply?: boolean;
+  /** Whether to suppress the message. Porting note: This option is not implemented in the Python version for the abstract `Agent` class, but is for {@link ConversableAgent}, leading to a type discrepancy, which is why this option is added here. */
+  silent?: boolean;
+};
+
+/**
+ * (In preview) An abstract class for AI agent.
+ * 
+ * An agent can communicate with other agents and perform actions.
+ * Different agents can differ in what actions they perform in the `receive` method.
+ */
+export abstract class Agent {
+
+  /**
+   * Construct an {@link Agent}.
+   * 
+   * @param name - The agent name.
+   */
+  constructor(
+    public name: string,
+  ) { }
+  
+  /**
+   * Send a message to another agent.
+   * 
+   * @param message - The message.
+   * @param recipient - The recipient agent.
+   * @param options - see {@link SendReceiveOptions}
+   */
+  abstract send(message: string | Message, recipient: Agent, options: SendReceiveOptions): Promise<void>;
+
+  /**
+   * Receive a message from another agent.
+   * 
+   * @param message - The message.
+   * @param sender - The sender agent.
+   * @param options - see {@link SendReceiveOptions}
+   */
+  abstract receive(message: string | Message, sender: Agent, options: SendReceiveOptions): Promise<void>;
+
+  /**
+   * Reset the agent.
+   */
+  abstract reset(): void;
+
+  /**
+   * Generate a reply based on the received messages.
+   * 
+   * @param messages - A list of messages received.
+   * @param sender - The sender agent.
+   * 
+   * @returns The generated reply. If `null`, no reply is generated.
+   */
+  abstract generateReply({ messages, sender }: { messages?: Message[], sender?: Agent }): Promise<string | Message | null>;
+
+};
