4aef283f7307743a5de3a3589f8f2f60aa533563
Vova Zakharov <vzakharov@gmail.com>
2023-11-12 12:15:48 +0300

    Create leisan.vue

---
+++ pages/leisan.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gradient-to-r from-purple-500 to-indigo-600 h-screen flex flex-col items-center justify-center">
+    <div class="absolute top-5 left-5 text-white text-xl">E</div>
+    <div class="flex space-x-10 text-white">
+      <h1 class="text-4xl">Лейсан Захарова</h1>
+      <h2 class="text-3xl">Экзистенциально-нарративный психолог</h2>
+    </div>
+    <div class="mt-10 text-white text-5xl">Объятие Существования</div>
+    <p class="mt-5 text-white text-base max-w-lg">
+      Занимаясь изучением глубин своей психики, вы сталкиваетесь с вопросами смысла, ценности жизни и выбора. Подход к психологии, основанный на исследовании человеческого опыта, его осмыслении и интерпретации, поможет вам найти гармонию в себе, осознать свои потребности и желания, а также научиться принимать своё существование со всеми его трудностями и радостями.
+    </p>
+    <div class="flex mt-10 space-x-5 text-white">
+      <button class="bg-white text-purple-600 px-5 py-2 rounded">Философия</button>
+      <button class="bg-white text-purple-600 px-5 py-2 rounded">Опыт</button>
+    </div>
+  </div>
+</template>
