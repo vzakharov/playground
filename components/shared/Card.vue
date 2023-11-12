77dc8349ac29469a5cf0bf456d47ef0cffbf4213
Vova Zakharov <vzakharov@gmail.com>
2023-11-12 15:00:09 +0300

    Update Card.vue

--- components/shared/Card.vue
+++ components/shared/Card.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="card-container">
     <div class="card">
-      <div class="card-title">
-        {{ title }}
+      <div class="card-header">
+        <img v-if="thumbnail" :src="thumbnail" class="card-thumbnail" />
+        <div class="flex flex-col">
+          <div>
+            <a v-if="url" :href="url" target="_blank" rel="noopener noreferrer"
+              v-text="title"
+              class="hover:underline"
+            />
+            <span v-else v-text="title" />
+          </div>
+          <div class="card-subtitle" v-if="subtitle">
+            {{ subtitle }}
+          </div>
+        </div>
       </div>
       <div class="card-content" 
         v-html="Marked.parse(content)" 
@@ -23,7 +35,6 @@
 </template>
 
 <script setup lang="ts">
-
   import { Marked } from '@ts-stack/markdown';
   import TextModal from './TextModal.vue';
   import { refForInstance } from './utils';
@@ -32,6 +43,9 @@
     title: string;
     modelValue: string; // content
     editOnDoubleClick?: boolean;
+    thumbnail?: string; // New prop
+    url?: string; // New prop
+    subtitle?: string; // New prop
   }>();
 
   const emit = defineEmits<{
@@ -47,27 +61,34 @@
   defineSlots<{
     footer: void;
   }>();
-
 </script>
 
 <style scoped lang="postcss">
-.card-container {
-  @apply w-full mt-6 mb-3
-}
+  .card-container {
+    @apply w-full mt-6 mb-3;
+  }
 
-.card {
-  @apply bg-white rounded-lg shadow-lg overflow-hidden;
-}
+  .card {
+    @apply bg-white rounded-lg shadow-lg overflow-hidden;
+  }
 
-.card-title {
-  @apply text-lg font-medium text-gray-800 p-4 py-2 border-b border-gray-200;
-}
+  .card-header {
+    @apply flex flex-row items-center gap-3 text-lg text-gray-800 p-4 py-2 border-b border-gray-200;
+  }
 
-.card-content {
-  @apply text-gray-700 p-4 pt-2;
-}
+  .card-subtitle {
+    @apply text-gray-400 text-sm;
+  }
 
-.card-footer {
-  @apply p-4 pt-2 border-t border-gray-200;
-}
+  .card-thumbnail {
+    @apply w-16 h-16 rounded-full object-cover;
+  }
+
+  .card-content {
+    @apply text-gray-700 p-4 pt-2;
+  }
+
+  .card-footer {
+    @apply p-4 pt-2 border-t border-gray-200;
+  }
 </style>
